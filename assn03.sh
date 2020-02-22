84a56873c58c3162b3f1f3c47deb79141d9f8c65
aiomolew <aiomolew@uark.edu>
2020-02-22 10:18:34 +0100

    assn03.sh

--- assn03.sh
+++ assn03.sh
@@ -1,5 +1,4 @@
 
-
 #! /bin/bash
 
 # assn03-1
@@ -23,4 +22,19 @@ ls -l .| egrep -c '*.tre' # ans: 14640
 ls -l .| egrep -c '*.sched'
 
 #assn03-6
+ls | egrep '*.fasta'| awk -F'[_]' '{print $1}'|sort > fasta.txt
+ls | egrep '*.tre'| awk -F'[_]' '{print $1}'|sort > tre.txt
+comm -23 fasta.txt tre.txt |wc -l  #ans: 445
 
+#assn03-7
+awk '/Program Return Code:/ {print FILENAME, " " $4}' *.sched| uniq|awk ' {if ($2 == '0') {count++; }} END {print count}'
+awk '/Program Return Code:/ {print FILENAME, " " $4}' *.sched| uniq|awk ' {if ($2 != '0') {count++; }} END {print count}'
+
+#assn03-8
+comm -23 fasta.txt tre.txt > notree.txt
+
+for word in $(cat notree.txt)
+ do 
+  command="write_raxml_job_script.py ${word}_alignment.fasta > ${word}_alignment.pbs"
+ echo $command
+ done
